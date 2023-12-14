0e7de61c3b8102a91cc02cac401fd529c04ec9a3
s4chin-verma <shivamvarma346@gmail.com>
2023-12-15 02:42:01 +0530

    Deployed to Vercel

---
+++ src/utils/APIRoutes.jsx
@@ -0,0 +1,12 @@
+export const host = 'https://to-do-list-backend.sachinverma28.repl.co';
+export const registerRoute = `${host}/api/auth/register`;
+export const loginRoute = `${host}/api/auth/login`;
+export const createTaskRoute = `${host}/api/route/tasks`; 
+export const deleteTaskRoute = `${host}/api/route/tasks`; 
+export const getAllTasksRoute = `${host}/api/route/tasks`; 
+export const isCompleted = `${host}/api/route/tasks`
+// Update task route
+// export const updateTaskRoute = (taskId) => `${host}/api/route/tasks/${taskId}`;
+
+// Delete task route
+// export const deleteTaskRoute = (taskId) => `${host}/api/route/tasks/${taskId}`;
